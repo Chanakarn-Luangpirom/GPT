9ac9576e208edcaa323b8e249a0560dd1d0f48ba
North <cl2369@cornell.edu>
2023-06-05 16:03:34 +0700

    Batch Data Loader

--- bigram.py
+++ bigram.py
@@ -41,3 +41,55 @@ val_data = data[n:]
 
 print('size of train:',len(train_data))
 print('size of val:',len(val_data))
+
+
+### Single Data Loader
+context_length = 8
+batch_size = 4
+
+x = train_data[:context_length]
+y = train_data[1:context_length+1]
+
+for i in range(context_length):
+    context = x[:i+1]
+    target = y[i]
+    print('input:', context)
+    print('target:', target)
+    print('--------')
+
+
+
+## Batch Data Loader
+torch.manual_seed(123)
+def generate_batch(split = 'train'):
+    if split == 'train':
+        data = train_data
+    else:
+        data = val_data
+
+    start_idxs = torch.randint(len(data)-context_length, (batch_size,))  # Random int with shape
+
+    x_batch = []
+    y_batch = []
+    for start_idx in start_idxs:
+        x = data[start_idx:start_idx+context_length]
+        y = data[start_idx+1:start_idx+context_length+1]
+
+        x_batch.append(x)
+        y_batch.append(y)
+    return torch.stack(x_batch),torch.stack(y_batch)
+
+x_batch,y_batch = generate_batch()
+print(x_batch)
+print(y_batch)
+
+for b in range(batch_size):
+    for i in range(context_length):
+        context = x_batch[b,:i+1]
+        target = y_batch[b,i]
+        print('input:', context)
+        print('target:', target)
+        print('--------')
+
+
+    
