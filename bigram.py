19fe1178bd1208a5564fff2948a4fbb183c5d499
North <cl2369@cornell.edu>
2023-06-05 15:31:58 +0700

    Encoder Decoder

--- bigram.py
+++ bigram.py
@@ -1,1 +1,27 @@
-print('test')
+with open('input.txt','r',encoding='utf-8') as f:
+    text = f.read()
+print('Length of Text:',len(text))
+
+
+characters = sorted(list(set(text)))
+print('Total Characters:', len(characters))
+
+encoder = {}
+decoder = {}
+for idx,char in enumerate(characters):
+    encoder[char] = idx
+    decoder[idx] = char
+
+def encode(text):
+    output = []
+    for char in text:
+        output.append(encoder[char])
+    return output
+def decode(indices):
+    output = []
+    for idx in indices:
+        output.append(decoder[idx])
+    return ''.join(output)
+
+print(encode('test encode'))
+print(decode(encode('test encode')))
